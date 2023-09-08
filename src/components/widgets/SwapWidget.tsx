11f0c1ef410b3b18ae1780862496ea5c84216b2c
joshkokatnur <jkokatnur@gmail.com>
2023-09-08 16:38:24 -0400

    Revert to e9ff23f (remove widget submodule)

---
+++ src/components/widgets/SwapWidget.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SwapWidget = () => {
+    return (
+        <div className="w-full flex items-center justify-center">
+        </div>
+    )
+}
+
+export default SwapWidget; 
